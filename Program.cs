e4628205100bde432550774b1d15932c829954ae
Travis Guthrie <Gu3@nowhwere>
2022-01-26 01:59:51 -0600

    updated console output

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
     C# Sharp Exercises: Compare two 
     string whether they are equal 
     or not.
+    Remove whitespace and ignore case
+
  */
 using System;
 
@@ -11,27 +13,36 @@ namespace Compare2StringsProj
     {
         static bool IsSame(string text1, string text2)
         {
-            text1 = text1.Trim();
-            text2 = text2.Trim();
+            text1 = text1.Trim().ToLower();
+            text2 = text2.Trim().ToLower();
             if (text1.Length == text2.Length)
             {
+                Console.Write("Same length is true");
                 for (int i = 0; i < text1.Length - 1; i++)
                 {
-                    if (text1[i] != text2[i]) return false;
+                    if (text1[i] != text2[i])
+                    {
+                        Console.Write(", but not same characters is ");
+                        return false;
+                    }
                 }
+                Console.Write(" & same Characters is ");
                 return true;
             }
+            Console.Write("Length or characters are same is ");
             return false;           
            
         }
         static void Main(string[] args)
         {
-            string testWord1 = "This is a test string";
+            string testWord1 = "This Is a test string";
             string testWord2 = "This is a test string ";
+            string testWord3 = "This is a test strung ";
             string testWord1b = "This is a bunch of words";
             string testWord2b = "This is nonsense-words: kibbers nz biztz";
 
             Console.WriteLine(IsSame(testWord1, testWord2));
+            Console.WriteLine(IsSame(testWord1, testWord3));
             Console.WriteLine(IsSame(testWord1b, testWord2b));
 
         }
